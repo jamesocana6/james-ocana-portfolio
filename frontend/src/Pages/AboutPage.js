d52e9242f7f472ecb9ee57be2648f5b62a0666c0
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-11-15 02:24:00 -0500

    Added about me paragraph

--- frontend/src/Pages/AboutPage.js
+++ frontend/src/Pages/AboutPage.js
@@ -4,7 +4,8 @@ import Skills from "../Components/Skills";
 const AboutPage = () => {
     return(
         <div>
-            <h1>AboutPage component</h1>
+            <h3>About Me</h3>
+            <p>I'm a software engineer with a strong growth mindset that graduated from General Assembly’s software engineering bootcamp. Prior to this program, I worked in a cross-functional position at Underwriters Laboratories as a project engineer for their growing automotive materials department. I was eager to learn everything and I expanded the team from 1 person to 5 people and got promoted to team lead. In my free time, I taught myself Java and grew to love solving problems and coding. I’m excited to utilize my strong collaboration and communication skills alongside my new passion for coding. </p>
             <Skills/>
         </div>
     )
