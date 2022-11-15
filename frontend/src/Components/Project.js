7854da72c8d3e847baeb07b5a4cc11fa2a0b0c76
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-11-15 02:28:06 -0500

    Styling added to project cards to fit technologies

--- frontend/src/Components/Project.js
+++ frontend/src/Components/Project.js
@@ -5,30 +5,36 @@ const Project = ({ projects }) => {
     let allProjects = projects.map((p, idx) => {
         return (
             <div className="col s6">
-                <div className="card large" key={idx}>
-                    <div class="card-image">
-                        <img style={{
-                            width: "auto",
-                            height: 300 + "px",
-                            marginLeft: "auto",
-                            marginRight: "auto"
-                        }} src={p.picture} />
+                <div className="card large" style={{
+                                height: 550 + "px",}} key={idx}>
+                    <div className="card-stacked">
+                        <div class="card-image">
+                            <img style={{
+                                width: "auto",
+                                height: 300 + "px",
+                                marginTop: 10 + "px",
+                                marginLeft: "auto",
+                                marginRight: "auto"
+                            }} src={p.picture} />
+                        </div>
+                        <div className="card-content">
+                            <span class="card-title">{p.project}</span>
+                            {p.description} <br />
+                            <div className="project-skill-list" style={{display: "flex", flexFlow: "wrap", minHeight: "max-content" }}>
+                            {p.technologies.map((tech, index) => {
+                                return (
+                                    <div className="project-skill" style={{minWidth: "max-content", marginTop: 3+"px", marginBottom: 3+"px"}}>
+                                    {tech}
+                                    </div>
+                                );
+                            })}
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-content">
-                        <span class="card-title">{p.project}</span>
-                        {p.description} <br />
-                        {p.technologies.map((tech, index) => {
-                            return (
-                                <div className="project-skill" style={{display: "inline"}}>
-                                {tech}
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <div className="card-action">
-                        <a size="small" href={p.github} target="_blank">Github</a>
-                        <a size="small" href={p.link} target="_blank">Live Link</a>
-                    </div>
+                        <div className="card-action">
+                            <a size="small" href={p.github} target="_blank">Github</a>
+                            <a size="small" href={p.link} target="_blank">Live Link</a>
+                        </div>
                 </div>
             </div>
         )
