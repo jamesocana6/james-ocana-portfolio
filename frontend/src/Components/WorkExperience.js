414b30f9cb75eb686039b29d94c1b1d45b2d235f
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-11-19 03:10:55 -0500

    styling work section

--- frontend/src/Components/WorkExperience.js
+++ frontend/src/Components/WorkExperience.js
@@ -1,24 +1,22 @@
 import React from "react";
 
 const WorkExperience = ({experience}) => {
-    let allProjects = experience.map((e, idx) => {
+    let work = experience.map((e, idx) => {
         return (
-            <div className="project-panel" key={"e"+idx}>
-                <div className="project-content">
-                    <span className="project-title">{e.project}</span>
-                    {e.description} <br />
-                    <div className="project-skill-list">
-                        {e.technologies.map((tech, index) => {
+            <div className="work-panel" key={"e"+idx}>
+                <div className="work-content">
+                    <span className="work-company">{e.company}</span>
+                    <span className="work-role">{e.role}</span>
+                    <span className="work-dates">{e.dates}</span>
+                    <span className="work-location">{e.location}</span>
+                    <div className="work-r-list">
+                        {e.responsibilities.map((r, index) => {
                             return (
-                                <span className="project-skill" key={"skill" + index}>
-                                    {tech}
+                                <span className="work-r" key={"r" + index}>
+                                    {r}
                                 </span>
                             );
                         })}
-                    </div>
-                    <div className="project-action">
-                        <a href={e.github} target="_blank">Github</a>
-                        <a href={e.link} target="_blank">Live Link</a>
                     </div>
                 </div>
             </div>
@@ -27,7 +25,7 @@ const WorkExperience = ({experience}) => {
     return(
         <div>
             <h3>Work Experience</h3>
-
+            {work}
         </div>
     )
 }
